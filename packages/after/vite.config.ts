a83411652c9508480cd98e432ec4abb6385db26a
이연정 <jjung222@douzone.com>
2026-03-08 19:19:32 +0900

    fix(after): 스토리북 이후 css 깨짐 현상

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -9,8 +9,10 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
